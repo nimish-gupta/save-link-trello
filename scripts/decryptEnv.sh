a3197a1f5bc66602e0f539288e501a1ca45c515a
Nimish <nimishgupta18396@gmail.com>
2020-04-10 18:55:05 +0530

    fix: added the env variable for mongo

--- scripts/decryptEnv.sh
+++ scripts/decryptEnv.sh
@@ -2,6 +2,8 @@
 
 set -eux
 
+ENCRYPTION_KEY=$1
+
 FILE_NAME=.env
 ENCRYPTED_FILE_NAME="$FILE_NAME.encrypted"
 openssl aes-256-cbc -d -in $ENCRYPTED_FILE_NAME -out $FILE_NAME -md md5 -k $ENCRYPTION_KEY
--- scripts/encryptEnv.sh
+++ scripts/encryptEnv.sh
@@ -2,8 +2,6 @@
 
 set -eux
 
-ENCRYPTION_KEY=$1
-
 FILE_NAME='.env'
 ENCRYPTED_FILE_NAME="$FILE_NAME.encrypted"
 openssl aes-256-cbc -e -in $FILE_NAME -out $ENCRYPTED_FILE_NAME -md md5 -k $ENCRYPTION_KEY
