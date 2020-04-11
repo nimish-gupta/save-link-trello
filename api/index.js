6cab1dcfa2d6d70f5578c118b082083af2e51b08
Nimish <nimishgupta18396@gmail.com>
2020-04-11 18:07:29 +0530

    chore: added morgan and nodemon

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const logger = require('morgan');
 
 const callback = require('./callback');
 const login = require('./login');
 
 const app = express();
+
+app.use(logger('short'));
 
 app.get('/api/login', login);
 app.get('/api/callback', callback);
