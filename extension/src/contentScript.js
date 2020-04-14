4512de1db0c036e507ac7b297945df95b5e39d15
Nimish <nimishgupta18396@gmail.com>
2020-04-14 20:34:37 +0530

    feat: send the new card to trello

--- extension/src/contentScript.js
+++ extension/src/contentScript.js
@@ -1,7 +1,20 @@
 (function () {
-	function notifyExtension(e) {
+	async function notifyExtension(e) {
 		if (e.altKey) {
-			console.log('hi');
+			e = e || window.event;
+			const store = await browser.storage.local.get([
+				'SAVE_LINK_LIST_ID',
+				'SAVE_LINK_AUTH_TOKEN',
+				'SAVE_LINK_AUTH_KEY',
+			]);
+			return fetch(
+				`https://api.trello.com/1/cards?idList=${store.SAVE_LINK_LIST_ID}&key=${
+					store.SAVE_LINK_AUTH_KEY
+				}&token=${store.SAVE_LINK_AUTH_TOKEN}&name=${e.target.getAttribute(
+					'href'
+				)}`,
+				{ method: 'POST' }
+			);
 		}
 	}
 
