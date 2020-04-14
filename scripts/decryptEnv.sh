2ba345920292b011ceda0bb88d9a44def91bb09b
Nimish <nimishgupta18396@gmail.com>
2020-04-14 20:52:16 +0530

    feat: added the encrypted env key

--- scripts/decryptEnv.sh
+++ scripts/decryptEnv.sh
@@ -4,7 +4,7 @@ set -eux
 
 ENCRYPTION_KEY=$1
 
-FILE_NAME=.env
+FILE_NAME=api/_config/constants.js
 ENCRYPTED_FILE_NAME="$FILE_NAME.encrypted"
 openssl aes-256-cbc -d -in $ENCRYPTED_FILE_NAME -out $FILE_NAME -md md5 -k $ENCRYPTION_KEY
 echo "Decryped file $FILE_NAME"
