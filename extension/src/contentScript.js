f5a47fd1878911947becfd4d79fdddf284d7275a
Nimish <nimishgupta18396@gmail.com>
2020-04-15 22:42:50 +0530

    refactor: make content script code more readable

--- extension/src/contentScript.js
+++ extension/src/contentScript.js
@@ -1,25 +1,45 @@
 (function () {
+	async function getHref(e) {
+		let event = e || window.event;
+		const href = event.target.getAttribute('href');
+		const r = new RegExp('^(?:[a-z]+:)?//', 'i');
+
+		if (r.test(href)) {
+			return href;
+		}
+		return `${window.location.protocol}//${window.location.host}${
+			href[0] === '/' ? href : '/' + href
+		}`;
+	}
+
+	async function createTrelloCard(params) {
+		const response = await fetch(
+			`https://api.trello.com/1/cards?idList=${params.idList}&key=${store.key}&token=${store.token}&name=${params.name}&desc=${params.desc}&urlSource=${params.urlSource}`,
+			{ method: 'POST' }
+		);
+		await response.json();
+	}
+
 	async function notifyExtension(e) {
 		if (e.altKey) {
-			e = e || window.event;
+			const name = document.title;
+			const desc = getHref(e);
+			const urlSource = desc;
+
 			const store = await browser.storage.local.get([
 				'SAVE_LINK_LIST_ID',
 				'SAVE_LINK_AUTH_TOKEN',
 				'SAVE_LINK_AUTH_KEY',
 			]);
-			const r = new RegExp('^(?:[a-z]+:)?//', 'i');
-			let href = e.target.getAttribute('href');
-			href = r.test(href)
-				? href
-				: `${window.location.protocol}//${window.location.host}${
-						href[0] === '/' ? href : '/' + href
-				  }`;
 
-			const response = await fetch(
-				`https://api.trello.com/1/cards?idList=${store.SAVE_LINK_LIST_ID}&key=${store.SAVE_LINK_AUTH_KEY}&token=${store.SAVE_LINK_AUTH_TOKEN}&name=${href}`,
-				{ method: 'POST' }
-			);
-			await response.json();
+			return createTrelloCard({
+				name,
+				desc,
+				urlSource,
+				idList: store.SAVE_LINK_LIST_ID,
+				key: store.SAVE_LINK_AUTH_KEY,
+				token: store.SAVE_LINK_AUTH_KEY,
+			});
 		}
 	}
 
